a364195163479bce3e02e47126f81ca15ca7278f
System Administrator <chrisdiantodoni@gmail.com>
2026-08-27 09:48:51 +0700

    feat: add mail components barrel export

---
+++ apps/paperless-user/src/components/mail/index.ts
@@ -0,0 +1,5 @@
+export { MailHeader } from "./MailHeader";
+export { MailList } from "./MailList";
+export { MailItem } from "./MailItem";
+export { MailDetail } from "./MailDetail";
+export { ApprovalDialog } from "./ApprovalDialog";
